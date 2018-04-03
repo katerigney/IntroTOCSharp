2b10b39fdf5c4b9cc37cea376b58a09a75db25ae
Rigney <krigney@proforma.com>
2018-04-03 14:30:51 -0400

    I have all but two variables displaying.

--- IntroTOCSharp/Program.cs
+++ IntroTOCSharp/Program.cs
@@ -10,9 +10,57 @@ namespace IntroTOCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+
+            //Create the following vairbles. Use the type you think would be the best
+
+            //number of dogs running around a dog park
+            int numDogs = 8;
+            Console.WriteLine($"There are {numDogs} dogs in the dog park.");
+
+
+            //the number of sandwhiches You can eat when you are hungry
+            double numSandwich = 1.5;
+            Console.WriteLine($"I can eat {numSandwich} sandwiches when I'm hungry.");
+
+
+            //the amount of money it costs to buy a nice dinner
+            //decimal dinnerCost = 37.73;
+            // Console.WriteLine($"It costs {dinnerCost} to buy a nice dinner.");
+
+            string[] car = new string[3] { "charcoal gray", "vintage Toyota", "Land Cruiser" };
+
+            // what color your dream car is
+            Console.WriteLine($"My dream car is {car[0]}.");
+
+
+            //what make your dream car is
+            Console.WriteLine($"My dream car is a {car[1]}.");
+
+
+            //what model your dream car is
+            Console.WriteLine($"My dream car is a {car[2]}.");
+
+
+            //todays date
+            // Console.WriteLine($"Today is {date}.");
+
+
+            //the first letter of you name
+            char firstInitial = 'K';
+            Console.WriteLine($"{firstInitial} is the first letter of my name.");
+
+
+            //the number of stars in the sky
+            string numStars = "infinite";
+            Console.WriteLine($"There are {numStars} stars in the sky.");
+
+
+            //Print out each variable in the middle of string describing the variable.
+            //ex: for the number of dogs in the park, I want you to print out There are 5 dogs running around the park
+
 
             Console.ReadLine();
+
         }
     }
 }
